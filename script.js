304e89cf9c916195492132f2fd3d1275cd34628d
Chen <liangchen_amelia@163.com>
2022-08-22 22:52:34 +0100

    sixth commit

--- script.js
+++ script.js
@@ -12,95 +12,54 @@ let euqalButton = document.getElementById('equal');
 //set up dot button and prevent the button from repetitive input
 let dotButton = document.getElementById('dot');
 
-
-//check if links work
-console.log(history);
-console.log(output);
-console.log(funcButton);
-console.log(numButton);
-console.log(clearAllButton);
-console.log(clearErrorButton);
-console.log(dotButton);
-console.log(euqalButton);
-
-
 //add function to variables
 let displays = "";
 let funcOperators = "";
-let sum = "";
-let calculation = "";
 
 let allowToInputOperator = false;
 let allowToInputDot = true;
 
-
 console.log("displays", displays);
 console.log("funcOperators", funcOperators);
+
 
 for(let i = 0; i < numButton.length; i++) {
     let number = numButton[i];
     number.onclick = function () {
         displays += number.innerHTML;
-        output.innerHTML = displays;   
+        output.innerHTML = displays;  
+        history.innerHTML = displays; 
 
         allowToInputOperator = true; 
     }
 }
-
-
 for (let i = 0; i < funcButton.length; i++) {
     let func = funcButton[i];
     func.onclick = function() {
         if(allowToInputOperator) {
-            funcOperators += func.innerHTML;
+            funcOperators = func.innerHTML;
             displays += funcOperators;
-            output.innerHTML = displays;  
+            output.innerHTML = displays;
+            history.innerHTML = displays;   
 
             allowToInputOperator = false;
             allowToInputDot = true
 
-        }
-             
+        }      
     }
 }
 
-
-
-/*add function to equal
-euqalButton.onclick = function() {
-    output.innerHTML = eval(displays);
-    funcOperators = "";
-}
-*/
-
+//Calculation
 euqalButton.onclick = function() {
 
-    const calculateNumbers = (num1, num2) => {
+    function calculate(toCalculate) {
+        return new Function('return ' + toCalculate)();
+      }
 
-        switch (calculation) {
-            case "add": 
-            sum = (Number(num1) + Number(num2).toString());
-            break;
-            case "subtract": 
-            sum = (Number(num1) - Number(num2).toString());
-            break;
-            case "multiply": 
-            sum = (Number(num1) * Number(num2).toString());
-            break;
-            case "divide": 
-            sum = (Number(num1) / Number(num2).toString());
-            break;
-        }
-        return sum;
-    }
-    sum.innerHTML = displays;
-    displays += funcOperators;
-    funcOperators += euqalButton.innerHTML;
-
+    output.innerHTML = calculate(displays);
+    history.innerHTML = calculate(displays);
     funcOperators = "";
-            
 }
-
 
 
 //add function to clear-all button
@@ -111,8 +70,8 @@ clearAllButton.onclick = function() {
 
 //add function to clear error button
 clearErrorButton.onclick = function() {
-    displays = "";
-    output.innerHTML = 0;
+    displays = clearErrorButton.value.substr (0,output.length-1);
+    output.innerHTML = displays;
 }
 
 //automatically add 0. when there's no number in the input box
@@ -127,5 +86,7 @@ dotButton.onclick = function() {
     }
     allowToInputDot = false;
 }
+
+
 
 
